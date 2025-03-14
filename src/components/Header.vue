8b5b796da71cc2c58fe176fcf99d49f12f50b722
jn.kim081 <jn.kim081@netive.co.kr>
2025-03-14 16:58:13 +0900

    메뉴 만들기2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,19 @@
 <template>
-    <v-app-bar :elevation="2">
+
+    <v-navigation-drawer v-model="drawer" location="right" temporary>
+        <UserInfoComp />
+        <v-list-item link title="음식"></v-list-item>
+        <v-list-item link title="영상"></v-list-item>
+        <v-list-item link title="여행"></v-list-item>
+        <v-list-item link title="반려동물"></v-list-item>
+    </v-navigation-drawer>
+
+    <v-app-bar :elevation="2"  color="primary">
         <template v-slot:prepend>
             <v-btn icon to="/">
                 <v-icon>mdi-arrow-left</v-icon>
             </v-btn>
-        </template>
+        </template>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
         <v-app-bar-title></v-app-bar-title>
         <template v-slot:append>
             <v-btn icon>
@@ -15,15 +24,6 @@
             </v-btn>
         </template>
     </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" location="right" temporary>
-        <UserInfoComp />
-        <v-list-item link title="음식"></v-list-item>
-        <v-list-item link title="영상"></v-list-item>
-        <v-list-item link title="여행"></v-list-item>
-        <v-list-item link title="반려동물"></v-list-item>
-    </v-navigation-drawer>
-
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
